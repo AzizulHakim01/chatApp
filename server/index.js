ac1b50b22faeabea5b58af11531fdc4e2796ebdb
Azizul Hakim <azizulhakimgps@gmail.com>
2023-04-01 16:34:44 +0600

    commit

--- server/index.js
+++ server/index.js
@@ -9,13 +9,13 @@ const server = http.createServer(app);
 
 const io = new Server(server, {
     cors: {
-        origin: "http://localhost:3000",
+        origin: "http://localhost:5173/",
         methods:["GET", "POST"]
     }
 })
 
 io.on("connected", (socket)=>{
-    console.log(socket.id);
+    console.log(`User COnnected: $(socket.id)`);
 
     socket.on("disconnect", ()=>{
         console.log("User Disconnected", socket.id)
